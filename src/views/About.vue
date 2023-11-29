6c058a548dd14da555719bdb11783e207419fed2
annamaria <szumiecam@gmail.com>
2023-11-29 11:31:30 +0100

    updates

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est iure voluptas
+      alias numquam fugiat quae modi quaerat aspernatur facere ratione,
+      provident aliquid laborum cum! At, error tempora! Fugiat, molestiae
+      corrupti minima officiis ratione optio veniam ipsam est fuga quae hic
+      soluta mollitia, ducimus, nesciunt nostrum voluptas esse tempore. Non
+      nobis nam, nostrum, deleniti natus assumenda dolorem corrupti in
+      voluptatibus ducimus quisquam, voluptates facilis atque minus iure. Neque,
+      enim consequatur laudantium placeat nostrum dolorum culpa reiciendis illum
+      beatae eius soluta aperiam odit dolorem, magnam voluptates alias ducimus
+      dolores quis tempore fugiat mollitia, quam voluptas. Corrupti sequi omnis
+      eaque iste vero deserunt!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est iure voluptas
+      alias numquam fugiat quae modi quaerat aspernatur facere ratione,
+      provident aliquid laborum cum! At, error tempora! Fugiat, molestiae
+      corrupti minima officiis ratione optio veniam ipsam est fuga quae hic
+      soluta mollitia, ducimus, nesciunt nostrum voluptas esse tempore. Non
+      nobis nam, nostrum, deleniti natus assumenda dolorem corrupti in
+      voluptatibus ducimus quisquam, voluptates facilis atque minus iure. Neque,
+      enim consequatur laudantium placeat nostrum dolorum culpa reiciendis illum
+      beatae eius soluta aperiam odit dolorem, magnam voluptates alias ducimus
+      dolores quis tempore fugiat mollitia, quam voluptas. Corrupti sequi omnis
+      eaque iste vero deserunt!
+    </p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic similique
+      ullam optio odit minima recusandae nihil reiciendis iste quia, quaerat
+      illum perferendis sint id blanditiis voluptatum voluptatibus consequatur
+      eius culpa consectetur doloremque assumenda nobis voluptatem! Magni illum
+      explicabo unde eveniet cupiditate harum atque ipsa. Natus laudantium
+      consequatur, modi cum ratione at doloribus voluptates, voluptas vero,
+      soluta autem rem reiciendis? Voluptates, iusto eum? Sint dolorum mollitia
+      nostrum in nemo. Nemo, beatae? Ratione laboriosam fugiat debitis non alias
+      animi vero qui, ad voluptate iste aperiam quasi, facere praesentium at
+      asperiores cupiditate voluptates enim tenetur, eaque cumque consequatur?
+      Saepe aut dolor omnis perspiciatis?
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic similique
+      ullam optio odit minima recusandae nihil reiciendis iste quia, quaerat
+      illum perferendis sint id blanditiis voluptatum voluptatibus consequatur
+      eius culpa consectetur doloremque assumenda nobis voluptatem! Magni illum
+      explicabo unde eveniet cupiditate harum atque ipsa. Natus laudantium
+      consequatur, modi cum ratione at doloribus voluptates, voluptas vero,
+      soluta autem rem reiciendis? Voluptates, iusto eum? Sint dolorum mollitia
+      nostrum in nemo. Nemo, beatae? Ratione laboriosam fugiat debitis non alias
+      animi vero qui, ad voluptate iste aperiam quasi, facere praesentium at
+      asperiores cupiditate voluptates enim tenetur, eaque cumque consequatur?
+      Saepe aut dolor omnis perspiciatis?
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic similique
+      ullam optio odit minima recusandae nihil reiciendis iste quia, quaerat
+      illum perferendis sint id blanditiis voluptatum voluptatibus consequatur
+      eius culpa consectetur doloremque assumenda nobis voluptatem! Magni illum
+      explicabo unde eveniet cupiditate harum atque ipsa. Natus laudantium
+      consequatur, modi cum ratione at doloribus voluptates, voluptas vero,
+      soluta autem rem reiciendis? Voluptates, iusto eum? Sint dolorum mollitia
+      nostrum in nemo. Nemo, beatae? Ratione laboriosam fugiat debitis non alias
+      animi vero qui, ad voluptate iste aperiam quasi, facere praesentium at
+      asperiores cupiditate voluptates enim tenetur, eaque cumque consequatur?
+      Saepe aut dolor omnis perspiciatis?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
